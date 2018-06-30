777b5fba8a5e03deb43a0e55281c2f5493cf1695
GUANGMINGSHI <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-06-30 22:02:26 +0900

    Update Basic.py

--- Basic.py
+++ Basic.py
@@ -1,13 +1,47 @@
+"""
+Opencvの基本：
+画像を読み込み、表示、保存、グレースケール化、カラーマップ、リサイズ、動画を再生する。
+"""
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
 # read image by channel 3
-imgBGR = cv2.imread("/Users/jimmy/Desktop/Python/sample.png", cv2.IMREAD_COLOR)
-imgRGB = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2RGB)
+img = cv2.imread("/Users/jimmy/Desktop/Python/sample.png", cv2.IMREAD_COLOR)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-cv2.imwrite("/Users/jimmy/Desktop/Python/sample_out.png", imgRGB)
+#resize the image
+img = cv2.resize(img, (100, 200))
+
+cv2.imwrite("/Users/jimmy/Desktop/Python/sample_out.png", img)
 
 #show the image
-plt.imshow(imgRGB)
+#画像をグレースケール化、カラーマップ
+plt.imshow(img, cmap="gray")
+#show image's colorbar
+plt.colorbar()
 plt.show()
+
+
+"""
+動画を再生する
+"""
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture("/Users/jimmy/Desktop/Python/Silicon.Valley.S05E04.mp4")
+
+#動画が終わるまで繰り返し
+while(cap.isOpened()):
+    #フレームの取得
+    ret, frame = cap.read()
+    #表示
+    cv2.imshow("image", frame)
+    #qが押されたら終了
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
