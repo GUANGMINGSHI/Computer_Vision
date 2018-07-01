562ada9c1b8e1c16ae1ef662a7c9f44048afaef0
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-07-01 14:08:21 +0900

    Update Basic image processing.py

--- Basic image processing.py
+++ Basic image processing.py
@@ -1,6 +1,6 @@
 """
 Opencvの基本：
-画像を読み込み、表示、保存、グレースケール化、カラーマップ、リサイズ、動画を再生する。
+画像を読み込み、表示、保存、生成グレースケール化、カラーマップ、リサイズ、動画を再生する。
 """
 
 import cv2
@@ -12,12 +12,14 @@ img = cv2.imread("/Users/jimmy/Desktop/Python/sample.png", cv2.IMREAD_COLOR)
 # BGR to RGB
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-#resize the image
+
+"""resize the image"""
 img = cv2.resize(img, (100, 200))
 
 cv2.imwrite("/Users/jimmy/Desktop/Python/sample_out.png", img)
 
-#show the image
+
+"""show the image"""
 #画像をグレースケール化、カラーマップ
 plt.imshow(img, cmap="gray")
 #show image's colorbar
@@ -25,9 +27,7 @@ plt.colorbar()
 plt.show()
 
 
-"""
-動画を再生する
-"""
+"""動画を再生する"""
 import cv2
 import numpy as np
 
@@ -45,4 +45,13 @@ while(cap.isOpened()):
 cap.release()
 cv2.destroyAllWindows()
 
+
+"""新しい画像を生成する"""
+import numpy as np
+from PIL import Image
+
+array = np.zeros([255, 255, 3], dtype=np.uint8)
+img = Image.fromarray(array)
+
+img.save('/Users/jimmy/Desktop/Python/testrgb.png')
 
