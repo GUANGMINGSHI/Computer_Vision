6cfe904f81129faa8a755c94316636f66e55116e
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-07-01 22:37:33 +0900

    Update Image's Center.py

--- Image's Center.py
+++ Image's Center.py
@@ -25,5 +25,25 @@ plt.imshow(img)
 plt.colorbar()
 plt.show()
 
+print(x, y)
+
 
 """ based on image calculate center """
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread("/Users/jimmy/Desktop/Python/sample.jpg", cv2.IMREAD_GRAYSCALE)
+
+# False: 密度不均一な画像の重心を求める
+# Ture: 密度均一な画像の重心を求める
+mu = cv2.moments(img, False)
+# ?Why 10/00, 01/00
+x, y = int(mu["m10"]/mu["m00"]), int(mu["m01"]/mu["m00"])
+
+cv2.circle(img, (x,y), 4, 100, 2, 4)
+plt.imshow(img)
+plt.colorbar()
+plt.show()
+
+print(x, y)
