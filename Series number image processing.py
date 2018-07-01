f46e04b4252f5d87195fa8c8e6595213e3c26387
GUANGMINGSHI <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-07-01 09:43:10 +0900

    Update Series number image processing.py

--- Series number image processing.py
+++ Series number image processing.py
@@ -6,19 +6,17 @@ import cv2
 import numpy as np
 
 for i in range(10):
+    #1桁の場合(0, 1, 2)
     img = cv2.imread(str(i)+ "/Users/jimmy/Desktop/Python/.png", cv2.IMREAD_GRAYSCALE)
-    # ゼロ埋め、3桁の場合（000、001）
+    #ゼロ埋め、3桁の場合（000、001）
     img = cv2.imread({0:03d}.png".format(i), cv2.IMREAD_GRAYSCALE")
 
 
 #連番ではないが、特定のディレクトリ内のファイルをすべて処理
 import cv2
 import numpy as np
-#use glob module
-import glob
-"""
-拡張子を指定: file = glob.glob("image/*.png")
-"""
-files = glob.glob("image/*")
+import glob #use glob module
+
+files = glob.glob("image/*") #拡張子を指定: file = glob.glob("image/*.png")
 for f in files:
     img = cv2.imread(f, cv2.IMREAD_GRAYSCALE)
